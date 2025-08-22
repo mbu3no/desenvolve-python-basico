226d9782ba35ce2de029b1c90df2e1cb03c97ea9
Matheus Bueno <matheussbueno10@hotmail.com>
2025-08-22 15:30:37 -0300

    Create aula1_questao4.py

---
+++ modulo5/aula1_questao4.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+agora = datetime.now()
+
+data = f"{agora.day:02d}/{agora.month:02d}/{agora.year}"
+hora = f"{agora.hour:02d}:{agora.minute:02d}"
+
+print(f"Data: {data}")
+print(f"Hora: {hora}")
