6c4476909c3e0976da6c5dd8d6db97b3d507db0d
Matheus Bueno <matheussbueno10@hotmail.com>
2025-08-22 13:47:48 -0300

    Create aula4_questao2.py

---
+++ modulo2/aula4_questao2.py
@@ -0,0 +1,4 @@
+fahrenheit = int(input("Digite a temperatura em Fahrenheit: "))
+celsius = int((fahrenheit - 32) * (5/9))
+print("{} graus Fahrenheit são {} graus Celsius.".format(fahrenheit, celsius))
+
