80d7a824a3b56597cf5500ad849acc9c743e483b
Matheus Bueno <matheussbueno10@hotmail.com>
2025-08-22 16:18:10 -0300

    Update aula2_questao3.py

--- modulo7/aula2_questao3.py
+++ modulo7/aula2_questao3.py
@@ -4,7 +4,6 @@ while True:
     frase = input('Digite uma frase (digite "fim" para encerrar): ')
     if frase.lower() == "fim":
         break
-    # Normaliza a frase
     frase_normalizada = ''.join(c.lower() for c in frase if c.isalnum())
     if frase_normalizada == frase_normalizada[::-1]:
         print(f'"{frase}" é palíndromo')
