9a3b3b635114f513d7a99aceb3455d87f1ed8c80
Matheus Bueno <matheussbueno10@hotmail.com>
2025-08-22 13:27:53 -0300

    Update aula2_questao1.py

--- modulo2/aula2_questao1.py
+++ modulo2/aula2_questao1.py
@@ -1,7 +1,7 @@
 a = 5
 b = 2
-print(type(a))   # <class 'int'>
-print(type(b))   # <class 'int'>
+print(type(a))  
+print(type(b))  
 c = a / b
-print(c)         # 2.5
-print(type(c))   # <class 'float'>
+print(c)        
+print(type(c))   
