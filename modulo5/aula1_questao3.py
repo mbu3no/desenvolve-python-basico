44c1b5ec0cdb720dcacf1c5e16e3dc53aab3c8e5
Matheus Bueno <matheussbueno10@hotmail.com>
2025-08-22 15:30:18 -0300

    Create aula1_questao3.py

---
+++ modulo5/aula1_questao3.py
@@ -0,0 +1,13 @@
+import random
+
+numero = random.randint(1, 10)
+
+while True:
+    palpite = int(input("Adivinhe o número entre 1 e 10: "))
+    if palpite < numero:
+        print("Muito baixo, tente novamente!")
+    elif palpite > numero:
+        print("Muito alto, tente novamente!")
+    else:
+        print(f"Correto! O número é {numero}.")
+        break
