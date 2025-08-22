6e07b2f3b00dd4ab94e400410b3fafc43c3bc5c4
Matheus Bueno <matheussbueno10@hotmail.com>
2025-08-22 13:52:39 -0300

    Create aula_questao3.py

---
+++ modulo3/aula_questao3.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite sua idade: "))
+jogou_3 = input("Já jogou pelo menos 3 jogos de tabuleiro? ") == "True"
+vitorias = int(input("Quantos jogos já venceu? "))
+
+apto = 16 <= idade <= 18 and jogou_3 and vitorias >= 1
+print("Apto para ingressar no clube de jogos de tabuleiro:", apto)
